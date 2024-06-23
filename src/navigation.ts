d3f0bb3b5bc9e8288fc3ea0a95dde394a3ae88df
Diogo Correia <me@diogotc.com>
2024-06-23 20:19:01 +0100

    feat: add page layout

---
+++ src/navigation.ts
@@ -0,0 +1,40 @@
+export const headerData = {
+  links: [
+    {
+      text: "About",
+      href: "/#about",
+    },
+    {
+      text: "Houses",
+      href: "/houses",
+    },
+    {
+      text: "Contact",
+      href: "/#contact",
+    },
+  ],
+};
+
+export const footerData = {
+  links: [
+    {
+      title: "Useful Links",
+      links: [
+        { text: "About us", href: "#" },
+        { text: "Houses", href: "#" },
+        { text: "Contact", href: "#" },
+      ],
+    },
+    {
+      title: "Legal",
+      widgets: ["complaints"],
+    },
+  ],
+  socialLinks: [
+    { ariaLabel: "Instagram", icon: "tabler:brand-instagram", href: "#" },
+    { ariaLabel: "Facebook", icon: "tabler:brand-facebook", href: "#" },
+  ],
+  footNote: `
+    Lorem Ipsum.
+  `,
+};
