d875e697df6c620d38b8beb85cec4a744194cb8d
Diogo Correia <me@diogotc.com>
2024-06-24 15:37:21 +0100

    feat: add hero and contacts section

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,20 @@ import { fileURLToPath } from "url";
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false })],
+  integrations: [
+    tailwind({ applyBaseStyles: false }),
+    icon({
+      include: {
+        tabler: ["*"],
+      },
+    }),
+  ],
 
   vite: {
     resolve: {
